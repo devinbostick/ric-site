2ea2b07e6fbf060aee9eedccde08212f6a8b48a2
Devin Bostick <devin.bostick@codesintelligence.com>
2025-10-03 12:47:07 -0600

    Initial site: static landing + artifacts

---
+++ tailwind.config.js
@@ -0,0 +1,6 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./app/**/*.{ts,tsx,mdx}","./components/**/*.{ts,tsx,mdx}","./content/**/*.{md,mdx}"],
+  theme: { extend: {} },
+  plugins: [require('@tailwindcss/typography')],
+};
